cd56559b21756b9773fd362997c8af423f9d2d78
RyanPoyyy <ryan.poy.2021@scis.smu.edu.sg>
2022-10-27 14:41:38 +0800

    connection to firebase

--- pet_finder/src/main.js
+++ pet_finder/src/main.js
@@ -4,12 +4,29 @@ import router from './router'
 import vuetify from './plugins/vuetify'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
+//backend stuff
+import "./firebase/index"
+import { 
+    getFirestore, collection, getDocs
+} from 'firebase/firestore'
+
+//connecting to firebase 
+const db=getFirestore()
+
+//get collection data
+const userRef=collection(db, 'Users')
+getDocs(userRef)
+.then((snapshot)=>{
+    console.log(snapshot.docs)
+    console.log("database connected")
+})
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   vuetify,
+  db,
   render: h => h(App),
   mounted() {
     AOS.init()
