8ca98a7ca884fec8587c3fec5d1cfc999c95db60
sclaireanalisa25 <claireas.2021@smu.edu.sg>
2022-11-12 15:22:59 +0800

    SEE HOW

--- pet_finder/src/views/MyMap.vue
+++ pet_finder/src/views/MyMap.vue
@@ -1,5 +1,10 @@
 <template>
-    <Map></Map>
+   <v-container>
+      <p class="text-h6 text-center font-weight-light brown--text text--darken-1">
+        To view the location where pets were lost/found, refer to our map below!
+      </p>
+      <Map></Map>
+   </v-container>
 </template>
         
 <script>
@@ -11,6 +16,7 @@ import Map from "../components/Map";
       data(){
         return {
           //no data for now
+          
         }
       },
       methods: {
