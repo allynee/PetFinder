2af30cf9000b66ceddff2869f84e72dc00cc3013
allynee <86574138+allynee@users.noreply.github.com>
2022-11-12 19:21:30 +0800

    Edited Data Table

--- pet_finder/src/components/DataTable.vue
+++ pet_finder/src/components/DataTable.vue
@@ -11,9 +11,9 @@
     :headers="headers" :items="pets" :items-per-page="10" 
     :search="search"
       class="elevation-0 brown lighten-5 text-primary text-center">
-        <template v-slot:[`item.petImage`]="{ item }">
+        <template v-slot:[`item.image`]="{ item }">
           <div class="p-2">
-            <v-img :src='getImgUrl(item.petImage)' contain max-height="250" max-width="225" class="my-5"></v-img>
+            <v-img :src='item.image' contain max-height="150" max-width="225" class="my-5"></v-img>
           </div>
         </template>
       </v-data-table>
@@ -30,15 +30,15 @@
         search: "",
         //data table
         headers: [
-          { text: "Pet Image", value: "petImage", sortable: false },
+          { text: "Pet Image", value: "image", sortable: false },
           { text: 'Pet Name', value: 'petName' },
-          { text: 'Last Seen Date', value: 'missingSince' },
-          { text: 'Last Seen Location', value: 'petLoc' },
+          { text: 'Last Seen Date', value: 'petDate' },
+          { text: 'Last Seen Location', value: 'petLocation' },
           { text: 'Pet Type', value: 'petType' },
           { text: 'Pet Breed', value: 'petBreed' },
-          { text: 'Pet Colour', value: 'petColour' },
+          { text: 'Pet Colour', value: 'petColor' },
           { text: 'Pet Gender', value: 'petGender' },
-          { text: 'Pet Collar Colour', value: 'collarColour' },
+          { text: 'Pet Collar Colour', value: 'collarColor' },
           { text: 'Pet Size', value: 'petSize' },
           { text: 'Pet Status', value: 'petStatus' },
         ],
